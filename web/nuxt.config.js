804462ab6a4c0c889c5c12030720c1b6d435cbbd
Adrian Hesketh <adrianhesketh@hushmail.com>
2021-01-24 11:54:41 +0000

    feat: added Serverless deployment

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'web',
@@ -24,11 +24,10 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
-  ],
+    // Doc: https://github.com/nuxt-community/eslint-module
+    process.env.NODE_ENV !== 'production' ? '@nuxtjs/eslint-module' : '',
+    process.env.NODE_ENV !== 'production' ? '@nuxtjs/stylelint-module' : '',
+  ].filter(Boolean),
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
---
+++ web/server.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const awsServerlessExpress = require('aws-serverless-express')
+const { Nuxt } = require('nuxt-start')
+const fastify = require('fastify')
+const fastifyStatic = require('fastify-static')
+const fastifyExpress = require('fastify-express')
+const config = require('./nuxt.config.js')
+
+/**
+ * Creates a fastify server with Nuxt middleware attached.
+ **/
+exports.createApp = async function start() {
+  // Define a list of mime types that we will serve.
+  // Let's serve everything using a wild card
+  const binaryTypes = ['*/*']
+
+  // Pass in our custom server function, which uses the aws-serverless-proxy
+  // to convert our default handler to a serverless compatible one.
+  const server = fastify({
+    serverFactory(handler) {
+      return awsServerlessExpress.createServer(handler, null, binaryTypes)
+    },
+  })
+  await server.register(fastifyExpress)
+
+  // Serve the `.nuxt/dist` folder using the `/_nuxt` prefix.
+  await server.register(fastifyStatic, {
+    root: path.join(__dirname, '.nuxt', 'dist'),
+    prefix: '/_nuxt/',
+  })
+
+  // In the Nuxt programmatic API
+  // We need to explicitly set the dev to false.
+  const nuxt = new Nuxt(Object.assign(config, { dev: false }))
+
+  // wait for nuxt to be ready.
+  await nuxt.ready()
+  server.use(nuxt.render)
+
+  // wait for fastify to be ready.
+  await server.ready()
+
+  return server
+}
